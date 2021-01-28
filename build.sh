2e1474811800a6d8379fcc297e09370774cd6b25
lytico <lytico@users.sourceforge.net>
2021-01-28 17:18:59 +0100

    build.sh: check if runs in MonoDevelop

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-BINDIR="$PWD/../../build/AddIns/Samsung.Netcoredbg"
+MDAddIns="$PWD/../../build/AddIns"
+if [ -d "$MDAddIns" ]; then
+  BINDIR="$MDAddIns/Samsung.Netcoredbg"
+else
+  BINDIR="$PWD/Samsung.Netcoredbg"
+fi
+
 if [ ! -d "build" ]; then mkdir build; fi
 if [ ! -d "$BINDIR" ]; then mkdir $BINDIR; fi
 cd build
